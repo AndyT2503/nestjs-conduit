0d7e0d3d2b91db8804bdf9de82f1881e89d93d7a
tuhoang <htu25399@gmail.com>
2023-05-30 20:59:41 +0700

    update validate update user dto

--- src/application/user/dto/update-user.dto.ts
+++ src/application/user/dto/update-user.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsOptional } from 'class-validator';
+import { IsEmail, ValidateIf } from 'class-validator';
 
 export class UpdateUserDto {
   @ApiProperty() readonly username: string;
-  @ApiProperty() @IsOptional() @IsEmail() readonly email: string;
+  @ApiProperty() @ValidateIf(e => !!e.email) @IsEmail() readonly email: string;
   @ApiProperty() readonly password: string;
   @ApiProperty() readonly image: string;
   @ApiProperty() readonly bio: string;
