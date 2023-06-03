982e46628ecf569abbc0299e2ac8faf1ddb7a723
tuhoang <htu25399@gmail.com>
2023-06-03 17:30:59 +0700

    move app.module & main.ts to user interface layer

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,12 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ArticleModule } from './api/controllers/article';
-import { ProfileModule } from './api/controllers/profile';
-import { UserModule } from './api/controllers/user';
-import { AuthMiddleware, AuthModule } from './infrastructure/auth';
-import { environmentConfiguration } from './infrastructure/environment-config';
-import { typeOrmConfigOptions } from './infrastructure/type-orm-config';
+import { ArticleModule } from './controllers/article';
+import { ProfileModule } from './controllers/profile';
+import { UserModule } from './controllers/user';
+import { AuthMiddleware, AuthModule } from '../infrastructure/auth';
+import { environmentConfiguration } from '../infrastructure/environment-config';
+import { typeOrmConfigOptions } from '../infrastructure/type-orm-config';
 
 @Module({
   imports: [
